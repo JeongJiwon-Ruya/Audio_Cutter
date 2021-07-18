9f9e7317e1eac040bd874cefccdc86aafbfd20a7
JeongJiwon <eetr3295@gmail.com>
2021-07-19 02:15:30 +0900

    normalize

--- silence_cutter.py
+++ silence_cutter.py
@@ -2,25 +2,37 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
 
-def match_target_amplitude(sound, target_dBFS):
-    change_in_dBFS = target_dBFS - sound.dBFS
-    return sound.apply_gain(change_in_dBFS)
-
-
 def silence_cutter(filename, split_filename="분할_", filetype="wav"):
     count = 1
-    file = AudioSegment.from_file(filename + ".wav", format=filetype)
-    # normalize
-    file = match_target_amplitude(file, -20.0)
+    file = AudioSegment.from_file(filename, format=filetype)
 
-    # split_on_silence(audio_segment, min_silence_len, silence_thresh, keep_silence)
+    # 전체 파일에서 가장 소리가 큰 값의 dBFS를 저장(normalize 위해)
+    max_dBFS = file.max_dBFS
+
+    # split_on_silence(
+    #   audio_segment : 파일,
+    #   min_silence_len : 탐지하는 silence의 최소 시간,
+    #   silence_thresh : silence로 정의되는 소리의 크기,
+    #   keep_silence : cut된 각 파일들 뒤에 붙는 silence의 길이)
     chunks = split_on_silence(file, 300, -50, 500)
-    print("분할될 파일의 개수 : ", len(chunks))
 
-    for chunk in chunks[0:]:
+    print("분할될 파일의 개수 : ", len(chunks)-1)
+
+    for chunk in chunks[1:]:
+        # cut될 각 파일들을 max_dBFS를 사용하여 normalize한다.
+        if max_dBFS >= chunk.max_dBFS:
+            chunk = chunk - (chunk.max_dBFS - max_dBFS)
+            # 확인용 print
+            # print("분할 파일의 max_dBFS :",chunk.max_dBFS)
+
+        # 각 파일들을 export
         chunk.export(split_filename + str(count) + '.' + filetype, format(filetype))
         count += 1
 
 
 if __name__ == '__main__':
-    silence_cutter("sample")
+
+    song = AudioSegment.from_file("sample_21.wav", format="wav")
+    print("파일의 dBFS값 :", song.max_dBFS)
+
+    silence_cutter("sample_21.wav")
